fa9fa8e8181a7ea2b83465443d4e41058398ed82
billvog <billvog04@gmail.com>
2021-03-28 16:31:51 +0300

    created controller package

---
+++ packages/controller/src/Controllers/RegisterController.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+interface RegisterControllerProps {}
+
+export const RegisterController: React.FC<RegisterControllerProps> = ({}) => {
+  return <div>hello from controllers</div>;
+};
