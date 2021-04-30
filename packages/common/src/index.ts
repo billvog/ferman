6d0781c727d87a67e88ec8a63971a36bcd23ed2b
billvog <billvog04@gmail.com>
2021-04-30 21:39:17 +0300

    Added empty yup schema

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -1,4 +1,8 @@
+import * as yup from "yup";
+
 export * from "./yupSchemas/User";
 export * from "./yupSchemas/Profile";
 export * from "./yupSchemas/Post";
 export * from "./yupSchemas/Comment";
+
+export const EmptySchema = yup.object().shape({});
