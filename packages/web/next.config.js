cc4d37a647fb8ce88e6478bb6adc3b394d54629f
billvog <billvog04@gmail.com>
2021-05-01 13:33:54 +0300

    Changed webpack from 5 to 4 again

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -1,7 +1,2 @@
 const withTM = require("next-transpile-modules")(["@ferman-pkgs/common", "@ferman-pkgs/controller"]);
-
-module.exports = withTM({
-  future: {
-    webpack5: true,
-  },
-})
+module.exports = withTM({})
